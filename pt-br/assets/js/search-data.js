b46a502230de8a576ce00d6fb7299a5653776d66
werbet <werbet@gmail.com>
2025-06-07 22:32:34 +0000

    Deploying to gh-pages from @ werbet/werbet.github.io@90892bb34008e8bb376911928173328223dc877f 🚀

--- pt-br/assets/js/search-data.js
+++ pt-br/assets/js/search-data.js
@@ -111,39 +111,11 @@ ninja.data = [{
           section: "Projetos",handler: () => {
               window.location.href = "/pt-br/projects/pt-br/6_project/";
             },},{
-        id: 'social-email',
-        title: 'Enviar um email',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Redes sociais',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Redes sociais',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Redes sociais',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Redes sociais',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Redes sociais',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/eriko", "_blank");
         },
       },{
           id: 'lang-en-us',
